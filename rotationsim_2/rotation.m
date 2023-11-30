b94f19a2a95343fbf2ac3534f294c8d50c6e34c6
jtwright206 <jtwr@ucdavis.edu>
2023-11-30 15:47:17 -0800

    Error fix

--- rotationsim_2/rotation.m
+++ rotationsim_2/rotation.m
@@ -2,20 +2,8 @@
 Ix = 4.59;
 Iy = 4.59;
 Iz = 5.3;
-J = [Ix 0 0; 0 Iy 0; 0 0 Iz]
+J = diag([Ix Iy Iz]);
 
-
-
-% initial euler angle conditions
-eul = [0 pi/36 0]
-
-% conversion to quaternions
-quat = eul2quat(eul)
-
-% initial angular rate
-omega = [0 0 0]
-% full initial conditions
-I = transpose([omega, quat(1:3)]);
 
 % goal conditions
 G = transpose([0 0 0 0 0 0]);
@@ -31,7 +19,6 @@ A = [0 0 0 0 0 0;
 B = [inv(J);
      zeros(3)];
 
-
 C = eye(6);
 
 D = zeros(6,3);
@@ -40,9 +27,21 @@ Q = diag([2.5 2.5 2.5 100 100 100]);
 
 R = diag([9 9 9]);
 
+% initial euler angle conditions
+eul = deg2rad([0 -45 0]);
+
+% conversion to quaternions
+quat = eul2quat(eul);
+
+% initial angular rate
+omega = [0 0 0];
+% full initial conditions
+I = transpose([omega, quat(2:4)]);
+
 % LQR calculion
 [K,S,P] = lqr(A,B,Q,R);
 
 open_system("modelsim")
-	
+    
 set_param("modelsim","SimulationCommand","Start")
+
