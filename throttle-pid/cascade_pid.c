32ff6fd4be56ccae2a3aececc96b967c3bda96e0
abhineetpal <57788065+abhineetpal@users.noreply.github.com>
2024-01-18 21:02:37 -0800

    Added code to update cascaded PID gains with time

--- throttle-pid/cascade_pid.c
+++ throttle-pid/cascade_pid.c
@@ -11,6 +11,27 @@ void initCascadedController(Cascaded_PID_Controller* controllerPtr) {
     controllerPtr->secondary = secondary;
 }
 
+void updateGains(Cascaded_PID_Controller* controllerPtr) {
+    double currentTime = 0.0; // FIXME: get actual timestamp in seconds
+    double multiplier = currentTime / controllerPtr->T;
+
+    if (multiplier < 1) {
+        PID_Controller primary = controllerPtr->primary;
+        PID_Controller secondary = controllerPtr->secondary;
+
+        primary.kP *= multiplier;
+        primary.kI *= multiplier;
+        primary.kD *= multiplier;
+
+        secondary.kP *= multiplier;
+        secondary.kI *= multiplier;
+        secondary.kD *= multiplier;
+
+        controllerPtr->primary = primary;
+        controllerPtr->secondary = secondary;
+    }
+}
+
 double calculateCascadedOutput(Cascaded_PID_Controller* controllerPtr, const double primarySetpoint, const double currentPrimaryState, const double currentSecondaryState) {
     PID_Controller primary = controllerPtr->primary;
     PID_Controller secondary = controllerPtr->secondary;
--- throttle-pid/cascade_pid.h
+++ throttle-pid/cascade_pid.h
@@ -4,11 +4,14 @@
 #include "pid.h"
 
 typedef struct Cascaded_PID_struct {
+    double T;
+
     PID_Controller primary;
     PID_Controller secondary;
 } Cascaded_PID_Controller;
 
-void initCascadedController(Cascaded_PID_Controller* controller);
+void initCascadedController(Cascaded_PID_Controller* controllerPtr);
+void updateGains(Cascaded_PID_Controller* controllerPtr);
 double calculateCascadedOutput(Cascaded_PID_Controller* controllerPtr, const double primarySetpoint, const double currentPrimaryState, const double currentSecondaryState);
 
 #endif
