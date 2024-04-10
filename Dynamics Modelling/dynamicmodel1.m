2afec4e5d155b44ab4905eacac1b2c6b0ea2d565
jtwright206 <jtwr@ucdavis.edu>
2024-04-09 17:59:51 -0700

    Update dynamicmodel1.m

--- Dynamics Modelling/dynamicmodel1.m
+++ Dynamics Modelling/dynamicmodel1.m
@@ -1,5 +1,6 @@
-t1 = 1.25
-t2 = 18
+
+t1 = 1.25 % throttle down
+t2 = 18 % throttle back up
 
 g = 10
 
@@ -7,13 +8,14 @@ m = 100
 
 W = m*g
 
-Ft1 = 1800
-Ft2 = 900
-Ft3 = 1100
 
-syms x
+Ft1 = 1800 % inital thrust
+Ft2 = 900 % thrust after t1 and before t2
+Ft3 = 1100 % thrust after t2
 
-Fx = piecewise(x < t1,Ft1,t1 < x < t2,Ft2,x>t2,Ft3)
+syms x % matlab syntax stuff
+
+Fx = piecewise(x < t1,Ft1,t1 < x < t2,Ft2,x>t2,Ft3) % thrust function
 
 a = (Fx-W)/m
 
@@ -21,9 +23,4 @@ v = int(a,0,x)
 
 s = int(v,0,x)
 
-%fplot(a)
-
-
-fplot(s,[0,40])
-
-
+fplot(s,[0,24])
