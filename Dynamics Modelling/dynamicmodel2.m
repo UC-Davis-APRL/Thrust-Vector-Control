a3f6466dceb6f0d910566270aba4880c62e45db7
jtwright206 <jtwr@ucdavis.edu>
2024-04-14 23:53:21 -0700

    New attitude control model

--- Dynamics Modelling/dynamicmodel2.m
+++ Dynamics Modelling/dynamicmodel2.m
@@ -6,14 +6,14 @@ mdot3 = -.9
 
 m0 = 100
 
-t1 = 1.7 % throttle down
-t2 = 30 % throttle back up
+t1 = 2.2 % throttle down
+t2 = 20 % throttle back up
 
 g = 10
 
 Ft1 = 1500 % inital thrust
-Ft2 = 700 % thrust after t1 and before t2
-Ft3 = 900 % thrust after t2
+Ft2 = 800 % thrust after t1 and before t2
+Ft3 = 920 % thrust after t2
 
 syms x % matlab syntax stuff
 
@@ -29,4 +29,6 @@ v = int(a,0,x)
 
 s = int(v,0,x)
 
-fplot(W,[0,40])
+fplot(a,[0,26.5])
+xlabel('t(s)') 
+ylabel('Vertical Displacement (m)')
