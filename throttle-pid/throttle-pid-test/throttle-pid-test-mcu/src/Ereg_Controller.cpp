d00db797880e061ae809ba27f2850cf71b750a88
abhineetpal <57788065+abhineetpal@users.noreply.github.com>
2024-05-24 17:11:38 -0700

    Fixed some stuff
    Added constraining on valve feedforward for safety
    Corrected servo offset
    Changed map from degree to servo pulse width (scaling was wrong)

--- throttle-pid/throttle-pid-test/throttle-pid-test-mcu/src/Ereg_Controller.cpp
+++ throttle-pid/throttle-pid-test/throttle-pid-test-mcu/src/Ereg_Controller.cpp
@@ -10,7 +10,7 @@ Ereg_Controller::Ereg_Controller(PID_Controller& pidController,
                                  double ullageVolumeThreshold): setpointController(pidController), valveServo(valveServo) {
     this->ullageVolumeThreshold = ullageVolumeThreshold;
 
-    servoOffset = 94;
+    servoOffset = -288;
 }
 
 void Ereg_Controller::updateGains(const double currentUllageVolume) {
@@ -35,8 +35,9 @@ void Ereg_Controller::setEnginePressure(
     
     double desiredCv = (a * pressureSetpoint) / (sqrt((b * pressureSetpoint) + c));
     double valveFeedforward = (7.84033 + ((-0.7624865 - 7.84033) / (1 + pow(desiredCv / 7243.604, 0.1969319)))) * 90;
+    valveFeedforward = constrain(valveFeedforward, 0, 90);
     double valveSetpoint = setpointController.calculate(currentPressure, pressureSetpoint) + valveFeedforward;
 
-    uint16_t servoPulseWidth = (uint16_t)(map(valveSetpoint, 0, 90, 1000, 1500)) + servoOffset;
+    uint16_t servoPulseWidth = (uint16_t)(map(valveSetpoint, 0, 90, 1000, 2000)) + servoOffset;
     valveServo.writeMicroseconds(servoPulseWidth);
 }
