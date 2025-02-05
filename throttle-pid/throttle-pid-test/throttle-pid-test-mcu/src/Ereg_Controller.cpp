b5e987ab54ae0fcf29cea0c35953a7db3b135aad
abhineetpal <57788065+abhineetpal@users.noreply.github.com>
2025-02-05 00:08:28 -0800

    Added new curve fit for DPF rocket

--- throttle-pid/throttle-pid-test/throttle-pid-test-mcu/src/Ereg_Controller.cpp
+++ throttle-pid/throttle-pid-test/throttle-pid-test-mcu/src/Ereg_Controller.cpp
@@ -34,10 +34,24 @@ void Ereg_Controller::setEnginePressure(
     double c = 2 * fluidDensity * tankPressure;
     
     double desiredCv = (a * pressureSetpoint) / (sqrt((b * pressureSetpoint) + c));
-    double valveFeedforward = (2349.578 + ((-0.4532749 - 2349.578) / (1 + pow(desiredCv / 139686000, 0.2498469)))) * 30;
-    valveFeedforward = constrain(valveFeedforward, 0, 30);
+
+    // Lander
+    // double valveFeedforward = (2349.578 + ((-0.4532749 - 2349.578) / (1 + pow(desiredCv / 139686000, 0.2498469)))) * 30;
+    // valveFeedforward = constrain(valveFeedforward, 0, 30);
+
+    // DPF
+    double valveFeedforward = -8.21992 + (56.45416 * desiredCv) - (105.6788 * pow(desiredCv, 2)) + (69.18414 * pow(desiredCv, 3));
+    valveFeedforward = constrain(valveFeedforward, 0, 5);
+    valveFeedforward *= 200;
+
     double valveSetpoint = setpointController.calculate(currentPressure, pressureSetpoint) + valveFeedforward;
 
-    int valveStepGoal = map(valveSetpoint, 0, 30, 0, 200);
+    // Lander
+    // int valveStepGoal = map(valveSetpoint, 0, 30, 0, 200);
+
+    // DPF
+    int valveStepGoal = valveSetpoint;
+
     valveStepper.step(valveStepGoal - currentPosition);
+    currentPosition = valveStepGoal;
 }
